169b576ab5235ecc34aad9e7a6d3ca75d3e996e4
David Buhauer <carbonex_92@hotmail.com>
2019-05-22 21:56:04 +0200

    Created basic auth for every request

--- Sources/App/Middleware/SecretMiddleware.swift
+++ Sources/App/Middleware/SecretMiddleware.swift
@@ -2,20 +2,26 @@ import Vapor
 
 final class SecretMiddleware: Middleware {
     
-    let secret: String
+    let username: String
+    let password: String
     
-    init(secret: String) {
-        self.secret = secret
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
     }
     
     func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
         
-        guard let bearerAuthorization = request.http.headers.bearerAuthorization else {
-            throw Abort(.unauthorized, reason: "Missing token")
+        guard let basicAuthorization = request.http.headers.basicAuthorization else {
+            throw Abort(.unauthorized, reason: "Missing authorization")
         }
         
-        guard bearerAuthorization.token == secret else {
-            throw Abort(.unauthorized, reason: "Wrong token")
+        guard basicAuthorization.username == username else {
+            throw Abort(.unauthorized, reason: "Wrong username")
+        }
+        
+        guard basicAuthorization.password == password else {
+            throw Abort(.unauthorized, reason: "Wrong password")
         }
         
         return try next.respond(to: request)
@@ -26,12 +32,19 @@ extension SecretMiddleware: ServiceType {
     
     static func makeService(for worker: Container) throws -> SecretMiddleware {
         
-        let secret: String
-        guard let envSecret = Environment.get("SECRET") else {
+        let username: String
+        let password: String
+        guard let envAuthUser = Environment.get("BASIC_AUTH_USER") else {
             let reason = "No SECRET set on environment."
             throw Abort(.internalServerError, reason: reason)
         }
-        secret = envSecret
-        return SecretMiddleware(secret: secret)
+        guard let envAuthPass = Environment.get("BASIC_AUTH_PASS") else {
+            let reason = "No SECRET set on environment."
+            throw Abort(.internalServerError, reason: reason)
+        }
+        username = envAuthUser
+        password = envAuthPass
+        
+        return SecretMiddleware(username: username, password: password)
     }
 }
