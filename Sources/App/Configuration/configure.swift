7112eac34506e518d0c4a25971eb474b095b9a26
David Buhauer <developer@dab-it.dk>
2021-03-17 17:51:10 +0100

    Added unverified tls support

--- Sources/App/Configuration/configure.swift
+++ Sources/App/Configuration/configure.swift
@@ -36,7 +36,7 @@ _ services: inout Services
     if env == .development {
         databases.enableLogging(on: .psql)
     }
-    let databaseConfig = PostgreSQLDatabaseConfig(url: databaseUrl)!
+    let databaseConfig = PostgreSQLDatabaseConfig(url: databaseUrl, transport: .unverifiedTLS)
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
